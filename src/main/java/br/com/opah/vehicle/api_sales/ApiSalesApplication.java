54e8004478c3d367a78a9363f847a132fe8726d9
Andreson <andreson.thiago@gmail.com>
2025-05-22 13:37:45 -0300

    setup project

---
+++ src/main/java/br/com/opah/vehicle/api_sales/ApiSalesApplication.java
@@ -0,0 +1,16 @@
+package br.com.opah.vehicle.api_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSalesApplication {
+
+	public static void main(String[] args) {
+
+		;
+		SpringApplication.run(ApiSalesApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/br/com/opah/vehicle/api_sales/ApiSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.opah.vehicle.api_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
